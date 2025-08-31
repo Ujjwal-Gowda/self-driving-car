701a512e6cdb9c3fe82c6d492c8185abbb48ddcd
ujjwal G <crystalninja0123@gmail.com>
2025-08-31 15:21:31 +0530

    dynamic traffic

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const carCanvas=document.getElementById("carCanvas");
 carCanvas.width=200;
 const networkCanvas=document.getElementById("networkCanvas");
 networkCanvas.width=300;
+let placement = -400; 
 
 const carCtx=carCanvas.getContext("2d");
 const networkCtx=networkCanvas.getContext("2d");
@@ -26,10 +27,22 @@ if(localStorage.getItem("bestBrain")){
         }
     }
 }
+setInterval(() => {
+  console.log("Runs every 2 seconds");
+  let lane=Math.floor(Math.random()*3);
+  placement -= 400;
+  console.log("New lane:", lane);
+  traffic.push(new Car(road.getLaneCenter(lane),placement,30,50,"Dummy",2));
+  if (traffic.length > 50) {
+    traffic.splice(0, 1); 
+  }
+}, 1000);
+
+
 const traffic=[
-    new Car(road.getLaneCenter(1),200,30,50,"Dummy",2),
-    new Car(road.getLaneCenter(0),-300,30,50,"Dummy",2),
-    new Car(road.getLaneCenter(2),-400,30,50,"Dummy",2)
+    // new Car(road.getLaneCenter(1),200,30,50,"Dummy",2),
+    // new Car(road.getLaneCenter(0),-300,30,50,"Dummy",2),
+    // new Car(road.getLaneCenter(2),-400,30,50,"Dummy",2)
 ];
 
 animate();
@@ -44,7 +57,7 @@ window.discard=function discard(){
 function generateCars(N){
     const cars=[];
     for(let i=0;i<N;i++){
-        cars.push(new Car(road.getLaneCenter(1),400,30,50,"AI",4))
+        cars.push(new Car(road.getLaneCenter(1),400,30,50,"AI",5))
     }
     return cars
     
